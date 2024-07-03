34a1904c25f3f70b7bc34cd0d5b2a6d3b8240bbe
Parth Kikani <parthkikani02@gmail.com>
2024-07-03 13:22:34 +0300

    complete '06-cache-management' extra credit 6

---
+++ src/utils/my-list-items.extra-6.js
@@ -0,0 +1,75 @@
+import { useQuery, useMutation, queryCache } from "react-query";
+import { client } from "./api-client";
+import { setQueryDataForBook } from "./books";
+
+const defaultMutationOptions = {
+  onSettled: () => queryCache.invalidateQueries("list-items"),
+  throwOnError: true,
+};
+
+function useListItems(user) {
+  const { data: listItems } = useQuery({
+    queryKey: "list-items",
+    queryFn: () =>
+      client(`list-items`, { token: user.token }).then(
+        (data) => data.listItems
+      ),
+    config: {
+      onSuccess(listItems) {
+        for (const listItem of listItems) {
+          setQueryDataForBook(listItem.book);
+        }
+      },
+    },
+  });
+
+  return listItems ?? [];
+}
+
+function useListItem(user, bookId) {
+  const listItems = useListItems(user);
+
+  const listItem = listItems.find((li) => li.bookId === bookId) ?? null;
+
+  return listItem;
+}
+
+function useUpdateListItem(user, options) {
+  return useMutation(
+    (updates) =>
+      client(`list-items/${updates.id}`, {
+        method: "PUT",
+        data: updates,
+        token: user.token,
+      }),
+    { ...defaultMutationOptions, ...options }
+  );
+}
+
+function useRemoveListItem(user, options) {
+  return useMutation(
+    ({ id }) =>
+      client(`list-items/${id}`, { method: "DELETE", token: user.token }),
+    { ...defaultMutationOptions, ...options }
+  );
+}
+
+function useCreateListItem(user, options) {
+  return useMutation(
+    ({ bookId }) =>
+      client(`list-items`, {
+        data: { bookId },
+        method: "POST",
+        token: user.token,
+      }),
+    { ...defaultMutationOptions, ...options }
+  );
+}
+
+export {
+  useListItem,
+  useListItems,
+  useUpdateListItem,
+  useRemoveListItem,
+  useCreateListItem,
+};
