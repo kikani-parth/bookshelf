e0b940d0701957333ddeedc54771102e5b9b117c
Parth Kikani <parthkikani02@gmail.com>
2024-07-04 13:07:13 +0300

    complete '07-context' extra credit 1

---
+++ src/context/my-auth-context.extra-1.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AuthContext = React.createContext();
+
+function useAuth() {
+  const context = React.useContext(AuthContext);
+
+  if (!context) {
+    throw new Error(`useAuth must be used within a AuthContext Provider`);
+  }
+
+  return context;
+}
+
+export { AuthContext, useAuth };
