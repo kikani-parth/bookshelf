b9be02337eb7c0955cb20ed4e6160c54707c5830
Parth Kikani <parthkikani02@gmail.com>
2024-07-07 14:47:10 +0300

    complete '11-Unit-Testing' exercise

---
+++ src/utils/__tests__/my-api-client.exercise.js
@@ -0,0 +1,79 @@
+import { server, rest } from "test/server";
+import { client } from "../api-client";
+
+const apiURL = process.env.REACT_APP_API_URL;
+
+beforeAll(() => server.listen());
+afterAll(() => server.close());
+afterEach(() => server.resetHandlers());
+
+test("calls fetch at the endpoint with the arguments for GET requests", async () => {
+  const endpoint = "test-endpoint";
+  const mockResult = { mockValue: "VALUE" };
+  server.use(
+    rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+      return res(ctx.json(mockResult));
+    })
+  );
+  const result = await client(endpoint);
+
+  // assert that the resolved value from the client call is correct
+  expect(result).toEqual(mockResult);
+});
+
+test("adds auth token when a token is provided", async () => {
+  const token = "FAKE_TOKEN";
+  let request;
+  const endpoint = "test-endpoint";
+  const mockResult = { mockValue: "VALUE" };
+
+  server.use(
+    rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+      request = req;
+      return res(ctx.json(mockResult));
+    })
+  );
+
+  await client(endpoint, { token });
+  expect(request.headers.get("Authorization")).toBe(`Bearer ${token}`);
+});
+
+test("allows for config overrides", async () => {
+  let request;
+  const endpoint = "test-endpoint";
+  const mockResult = { mockValue: "VALUE" };
+
+  server.use(
+    rest.put(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+      request = req;
+      return res(ctx.json(mockResult));
+    })
+  );
+
+  const customConfig = {
+    method: "PUT",
+    headers: { "Content-Type": "fake-type" },
+  };
+
+  await client(endpoint, customConfig);
+
+  // verify the request had the correct properties
+  expect(request.headers.get("Content-Type")).toBe(
+    customConfig.headers["Content-Type"]
+  );
+});
+
+test("when data is provided, it is stringified and the method defaults to POST", async () => {
+  const data = { fakeKey: "fakeValue" };
+  const endpoint = "test-endpoint";
+
+  server.use(
+    rest.post(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+      return res(ctx.json(req.body));
+    })
+  );
+
+  const result = await client(endpoint, { data });
+
+  expect(result).toEqual(data);
+});
