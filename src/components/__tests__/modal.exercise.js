7fad98e749075c578179ab7307b4d702b56628b2
Parth Kikani <parthkikani02@gmail.com>
2024-07-08 11:08:44 +0300

    complete '12-Testing Hooks and Components' extra credit 1

--- src/components/__tests__/modal.exercise.js
+++ src/components/__tests__/modal.exercise.js
@@ -1,11 +1,35 @@
-// 🐨 you're gonna need this stuff:
-// import {Modal, ModalContents, ModalOpenButton} from '../modal'
+import * as React from "react";
+import { render, screen, within } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Modal, ModalOpenButton, ModalContents } from "../modal";
 
-test.todo('can be opened and closed')
-// 🐨 render the Modal, ModalOpenButton, and ModalContents
-// 🐨 click the open button
-// 🐨 verify the modal contains the modal contents, title, and label
-// 🐨 click the close button
-// 🐨 verify the modal is no longer rendered
-// 💰 (use `query*` rather than `get*` or `find*` queries to verify it is not rendered)
-// 💰 Remember all userEvent utils are async, so you need to await them.
+test("can be opened and closed", async () => {
+  const label = "Modal Label";
+  const title = "Modal Title";
+  const content = "Modal content";
+
+  // render the Modal, ModalOpenButton, and ModalContents
+  render(
+    <Modal>
+      <ModalOpenButton>
+        <button>Open</button>
+      </ModalOpenButton>
+      <ModalContents aria-label={label} title={title}>
+        <div>{content}</div>
+      </ModalContents>
+    </Modal>
+  );
+  await userEvent.click(screen.getByRole("button", { name: /open/i }));
+
+  // verify the modal contains the modal contents, title, and label
+  const modal = screen.getByRole("dialog");
+  expect(modal).toHaveAttribute("aria-label", label);
+  const inModal = within(modal);
+  expect(inModal.getByRole("heading", { name: title })).toBeInTheDocument();
+  expect(inModal.getByText(content)).toBeInTheDocument();
+
+  await userEvent.click(inModal.getByRole("button", { name: /close/i }));
+
+  //verify the modal is no longer rendered
+  expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+});
